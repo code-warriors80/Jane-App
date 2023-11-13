166709a58d4654c01c554caa9f8202bfff578f12
youngsavage-codes <muctarmohammed07@gmail.com>
2023-11-13 12:43:54 +0100

    update

--- navigation.tsx
+++ navigation.tsx
@@ -7,6 +7,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from './screens/home';
 import OnboardingScreen from './screens/onboarding';
 import ProfileScreen from './screens/profile';
+import LoginScreen from './screens/login';
+import RegisterScreen from './screens/register';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +19,8 @@ const Navigation = () => {
             <Stack.Screen name="OnBoarding" component={OnboardingScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
         </Stack.Navigator>
     </ NavigationContainer>
   )
