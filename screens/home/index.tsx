d59b45a7d865cf457b29cd989d76d018d44d5950
codewarriors80 <codewarriors02@gmail.com>
2023-11-16 07:58:12 +0100

    update

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import tailwind from 'twrnc'
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={tailwind`flex-1 items-center justify-center`}>
       <Text>Home</Text>
     </View>
   )
