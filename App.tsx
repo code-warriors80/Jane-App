25b7d7a6fd373efc2e382fc5ca9f14e931f93b4a
codewarriors80 <codewarriors02@gmail.com>
2023-11-16 07:56:26 +0100

    update

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
 import Navigation from './navigation';
+import tailwind from 'twrnc'
 
 export default function App() {
   return (
-    <View>
+    <View style={tailwind`flex-1`}>
       <Navigation />
     </View>
   );
