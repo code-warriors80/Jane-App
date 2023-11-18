890b31019ac375cfe149829e56767740a3037989
codewarriors80 <codewarriors02@gmail.com>
2023-11-18 08:33:57 +0100

    update

--- components/homeHeader/homeHeader.tsx
+++ components/homeHeader/homeHeader.tsx
@@ -1,10 +1,20 @@
 import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import FIcon from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import dp from '../../assets/matheus-ferrero-W7b3eDUb_2I-unsplash.jpeg'
 import tailwind from 'twrnc'
 
 const HomeHeader = () => {
+    const [loved, setLoved] = useState(false)
+    const Love = () =>{
+        if(loved === false){
+            setLoved(true)
+        }else
+        {
+            setLoved(false)
+        }
+    }
   return (
     <View style={tailwind`px-6`}>
             <View style={tailwind`flex-row items-center justify-between pt-8 pb-4`}>
@@ -13,8 +23,8 @@ const HomeHeader = () => {
                         <TouchableOpacity style={tailwind`border-2 border-gray-200 p-3 rounded-xl`}>
                             <FIcon name='bell' size={15} color='black'/>
                         </TouchableOpacity>
-                        <TouchableOpacity style={tailwind`border-2 border-gray-200 p-3 rounded-xl`}>
-                            <FIcon name='heart' size={15} color='black'/>
+                        <TouchableOpacity style={tailwind`border-2 border-gray-200 p-3 rounded-xl`} onPress={Love}>
+                            {loved !== false ?  <Icon name='heart' size={15} color='#f53488'/> :  <FIcon name='heart' size={15} color='black'/> }
                         </TouchableOpacity>
                 </View>
             </View>
