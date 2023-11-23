802438ecd21cb1c50a9d8925c0490a22c01f5a19
codewarriors80 <codewarriors02@gmail.com>
2023-11-23 10:03:59 +0100

    update

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -18,55 +18,55 @@ const LoginScreen: React.FC = () => {
   };
 
   return (
-    <View style={tailwind` justify-center p-8`}>
+    <View style={tailwind``}>
 
       <View style={tailwind`items-center`}>
         <Text style={tailwind`text-3xl mt-10 `}>Login</Text>
         <Image
           source={require('../../assets/images/pic2.png')}
-          style={tailwind`w-80 h-80 mb-4`}
-          resizeMode="contain"
-        />
+          style={tailwind`w-50 h-50 mb-4`}/>
       </View>
 
-      <View style={tailwind`mb-4`}>
-        <View style={tailwind`flex-row items-center`}>
-          <Icon name="mail" size={20} style={tailwind`text-gray-400 mr-2`} /> 
-          <TextInput
-            style={tailwind`border rounded-3 px-3  border-gray-400 py-3 w-80`}
-            value={email}
-            onChangeText={setEmail}
-            placeholder="example@gmail.com"
-          />
-        </View>
-      </View>
+      <View style={tailwind`mx-5`}>
+          <View style={tailwind`mb-4`}>
+            <View style={tailwind`px-4 flex-row items-center gap-2 border border-gray-400 rounded-3`}>
+              <Icon name="mail" size={20} style={tailwind`text-gray-400`} /> 
+              <TextInput
+                style={tailwind` px-3  py-3 w-80`}
+                value={email}
+                onChangeText={setEmail}
+                placeholder="example@gmail.com"
+              />
+            </View>
+          </View>
 
-      <View style={tailwind`mb-4`}>
-        <View style={tailwind`flex-row items-center`}>
-          <Icon name="lock-closed" size={20} style={tailwind`text-gray-400 mr-2`} /> 
-          <TextInput
-            style={tailwind`border rounded-3 px-3  border-gray-400 py-3 w-80`}
-            value={password}
-            onChangeText={setPassword}
-            placeholder="Enter your password"
-            secureTextEntry={true}
-          />
-        </View>
-      </View>
+          <View style={tailwind`mb-2`}>
+            <View style={tailwind`px-4 flex-row items-center gap-2 border border-gray-400 rounded-3`}>
+              <Icon name="lock-closed" size={20} style={tailwind`text-gray-400`} /> 
+              <TextInput
+                style={tailwind`py-3 flex-1`}
+                value={password}
+                onChangeText={setPassword}
+                placeholder="Enter your password"
+                secureTextEntry={true}
+              />
+            </View>
+          </View>
 
-      <TouchableOpacity style={tailwind`bg-pink-500 py-3 w-80 h-14 mx-6 px-3 rounded-md items-center`} onPress={handleSignup}>
-        <Text style={tailwind`text-white text-lg  font-semibold`}>Sign in</Text>
-      </TouchableOpacity>
+          <TouchableOpacity style={tailwind`rounded-md mb-2`} >
+            <Text style={tailwind`text-lg text-right `}>forgot password?</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity style={tailwind`py-3 px-6 ml-40 rounded-md mb-4`} >
-        <Text style={tailwind`text-lg `}>forgot password?</Text>
-      </TouchableOpacity>
-
-      <View style={tailwind`flex-row items-center justify-center gap-2`}>
-        <Text>New To Our App</Text>
-        <TouchableOpacity onPress={() =>  navigation.navigate("RegisterStack")}>
-          <Text>Register</Text>
+        <TouchableOpacity style={tailwind`bg-pink-500 py-3  px-3 rounded-md mb-3`} onPress={handleSignup}>
+            <Text style={tailwind`text-white text-lg  font-semibold text-center`}>Sign in</Text>
         </TouchableOpacity>
+
+        <View style={tailwind`flex-row items-center justify-center gap-1`}>
+          <Text>New To Our App</Text>
+          <TouchableOpacity onPress={() =>  navigation.navigate("RegisterStack")}>
+            <Text style={tailwind`text-[#ec589c]`}>Register</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
